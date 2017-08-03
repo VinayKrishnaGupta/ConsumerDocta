1c6ed09f3f419b2869b2f7f70f0b33690de1666f
VinayKrishnaGupta <vinaygupta.iitkgp@gmail.com>
2017-08-03 10:19:26 +0530

    close case

---
+++ Docta Consumer/CloseCasesViewController.swift
@@ -0,0 +1,53 @@
+    //
+//  CloseCasesViewController.swift
+//  Docta Consumer
+//
+//  Created by RSTI E-Services on 02/08/17.
+//  Copyright © 2017 RSTI E-Services. All rights reserved.
+//
+
+import UIKit
+
+class CloseCasesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+        // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell : UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = "Back Pain"
+        cell.detailTextLabel?.text = "Dr. Thomas S."
+        
+        return cell
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
